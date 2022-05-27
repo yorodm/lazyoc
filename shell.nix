7da96849670ef4e2eeb44ff3d18e436cdd400b8d
Yoandy Rodriguez <yoandy.rmartinez@gmail.com>
2022-05-27 22:29:26 +0300

    Updated: 2022-05-27 22:29:26

---
+++ shell.nix
@@ -0,0 +1,8 @@
+{ pkgs ? import <nixpkgs> { } }:
+
+let
+  unstable = import (builtins.fetchTarball
+    "https://github.com/nixos/nixpkgs/tarball/nixpkgs-unstable") { };
+in pkgs.mkShell {
+  buildInputs = [ pkgs.rustc pkgs.cargo unstable.cargo-generate ];
+}
